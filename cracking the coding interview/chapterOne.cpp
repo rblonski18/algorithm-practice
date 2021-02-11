42d734bf1700c928b686162c9d83ffd848fc9a2e
Rory Blonski <blonski@usc.edu>
2021-02-10 23:10:50 -0500

    Add files via upload

--- cracking the coding interview/chapterOne.cpp
+++ cracking the coding interview/chapterOne.cpp
@@ -136,3 +136,76 @@ public:
 	}
 }
 
+// 1.5 - given two strings, write a method to determine whether
+// the strings are "one edit" away from one another - insert
+// remove, or replace a character. 
+//
+// move pointers through the word comparing letters. the 
+// letters in the word should only differ by one, but not 
+// necessarily in the same indices. 
+
+class Solution1_5 {
+public:
+	bool oneEditAway(string str1, string str2) {
+		if(abs(str1 - str2) > 1) return false; 
+
+		string first = str1.size() < str2.size() ? str1 : str2;
+		string second = str1.size() < str2.size() ? str2 : str1;
+
+		int i1 = 0, i2 = 0;
+		bool diff = false;
+		while(i2 < second.size() && i1 < first.size()) {
+			if(first[i1] != second[i2]) {
+				if(diff) return false;
+				diff = true;
+
+				if(first.size() == second.size()) i1++;
+			} else {
+				i1++;
+			}
+			i2++;
+		}
+		return true;
+	}
+}
+
+// 1.6 - string compression. given a string, such as "aaabcccaaa",
+// write a method to compress it such that it becomes "a3b1c3a3". 
+// if the length of the compressed is not less than the original
+// string, just return the original string. 
+//
+// My idea wasn't in CTCI but I think it makes more sense. Begin 
+// iteration through the loop. Grab current character and begin a
+// counter. Create a while loop that says "while the current 
+// character is a part of the character group we're looking at, 
+// move the iterator forward and update the count." This brings i 
+// forward until it meets a 'foreign' character. Update i backwards
+// one because it will iterate over again when the for loop 'loops'
+// over again, and we want to capture that foreign character. 
+// Append current character and it's count onto the string. 
+
+class Solution1_6 {
+public: 
+	string stringCompression(string toCompress) {
+		int len = toCompress.size();
+		string compressed;
+
+		for(int i = 0; i < len; i++) {
+			char current = toCompress[i];
+			int count = 0;
+
+			while(toCompress[i] == current) {
+				count++;
+				i++;
+			}
+			i--;
+
+			compressed += current;
+			compressed += to_string(i);
+		}
+
+		if(compressed.size() >= len) return toCompress;
+		else return compressed;
+	}
+}
+
