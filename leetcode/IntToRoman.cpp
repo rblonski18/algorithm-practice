0ec8b2b1fdb1665d63e63877083002408a427cf9
Rory Blonski <blonski@usc.edu>
2021-03-14 14:04:59 -0400

    Add files via upload

---
+++ leetcode/IntToRoman.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        // sort roman numeral numbers in descending order, 
+        // parse through string arrays, subtracting greatest possible sum,
+        // add corresponding roman numeral to output string. 
+        
+        string res;
+        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        for(int i=0; num != 0; i++)
+        {
+            while(num >= val[i])
+            {
+                num -= val[i];
+                res += sym[i];
+            }
+        }
+        
+        return res;
+    }
+};
